f31d68679c7d4343afd18ff6af969af4e5570cd1
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-12-01 19:15:47 +0100

    day2task2

--- day1Task1/day1task0.py
+++ day1Task1/day1task0.py
@@ -9,9 +9,18 @@ with open("C:/Users/adams/Documents/GitHub/Advent_Calendar/day1Task1/data.txt", 
         else:
             elves[-1]+=int(line.strip("\n"))
 
-    bigges = 0
+    biggest = 3
+    biggest2 =2
+    biggest3 = 1
     for e in elves:
-        if e >=bigges:
-            bigges = e
-            
-    print(bigges)
+        if e >=biggest:
+            biggest3 = biggest2
+            biggest2 = biggest
+            biggest = e
+        elif e >=biggest2:
+            biggest3 = biggest2
+            biggest2 = e
+        elif e >=biggest3:
+            biggest3 = e
+
+    print(biggest3+biggest2+biggest)
