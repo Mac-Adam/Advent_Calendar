9996e86d75112405b73d112dbd95ba816e552185
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-12-20 09:21:32 +0100

    day19
    computer change

--- day19/day19.py
+++ day19/day19.py
@@ -1,6 +1,6 @@
 class blueprint:
-    def __init__(self):
-        None
+    def __init__(self,costs):
+        self.costs = costs
 
 
 def getBlueprints(blueprints):
@@ -13,12 +13,39 @@ def getBlueprints(blueprints):
             robotCosts.append((int(robotCostsLines[1].split("Each clay robot costs")[-1].strip("ore")),0,0))
             robotCosts.append((int(robotCostsLines[2].split("Each obsidian robot costs")[-1].split("ore and")[0]),int(robotCostsLines[2].split("Each obsidian robot costs")[-1].split("ore and")[1].strip("clay")),0))
             robotCosts.append((0,int(robotCostsLines[3].split("Each geode robot costs")[-1].split("ore and")[0]),int(robotCostsLines[3].split("Each geode robot costs")[-1].split("ore and")[1].strip("obsidian"))))
-            print(robotCosts)
+            blueprints.append(blueprint(robotCosts))
+
+def canBuild(resources,cost):
+    can = True
+    for i,c in enumerate(cost):
+        if c>resources[i]:
+            can = False
+    return can
+
+
+
+
+def getBuildPossibilities(possibilities,resources,blue):
+    for robot in blue.costs:
+
+
+
+def evaluateBluePrint(blue,robots=[1,0,0,0],resources=[0,0,0,0],timeLeft = 24):
+    newResources = []
+    for idx in range(4):
+        newResources.append(resources[idx] + robots[idx])
+    possibleRobotsBuilds = [0,0,0,0]
+
+
+
+
 
 def task1():
     blueprints = []
-
     getBlueprints(blueprints)
+
+
+
 
 if __name__ == "__main__":
     task1()
