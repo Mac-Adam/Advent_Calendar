457d71295c505e35381e517736f9dccb1064b260
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-12-24 20:14:12 +0100

    day23
    I want to DIE
    ... not realy but this was frustrating
    my error was thinking N is (0,1 ) not (0,-1)

--- day23/day23.py
+++ day23/day23.py
@@ -1,150 +1,89 @@
-class Elf:
 
-    def __init__(self,pos):
-        self.pos = pos
-        self.wants = None
-
-def getElves(elves,map):
+def getElves(elves):
     with open('day23/data.txt','r') as file:
-        onlyValid = []
         for y,line in enumerate(file):
             line = line.strip('\n')
-            row = []
             for x,c in enumerate(line):
                 if c == '#':
-                    elves.append(Elf([x,y]))
-                    row.append(True)
-                else:
-                    row.append(False)
-            onlyValid.append(row)
-        width = len(onlyValid[0])
-        height = len(onlyValid)
-        for _ in range(height):
-            map.append([False]*3*width)
-        for line in onlyValid:
-            map.append([False]*width+line+[False]*width)
-        for _ in range(height):
-            map.append([False]*3*width)
-        for elf in elves:
-            elf.pos[0] += width
-            elf.pos[1] += height
+     
+                    elves.add((x,y))           
 
-def validForMovement(pos,map):
-    toCheck = [
-        (1,1),
-        (0,1),
-        (-1,1),
-        (1,0),
-        (-1,0),
-        (1,-1),
-        (0,-1),
+def moveElves(elves,turn):
+    moved = False
+    checks = [
+        [(-1,-1),(0,-1),(1,-1)],
+        [(-1,1),(0,1),(1,1)],
+        [(-1,1),(-1,0),(-1,-1)],
+        [(1,1),(1,0),(1,-1)],
+    ]
+    ofsets = [
         (-1,-1),
+        (0,-1), 
+        (1,-1), 
+        (1,1), 
+        (0,1), 
+        (-1,1), 
+        (-1,0), 
+        (1,0)
         ]
-    res = False
-    for check in toCheck:
-        res = res or map[pos[1]+check[1]][pos[0]+check[0]]
-    return res
 
-def checkMovement(pos,type,map):
-    cheks = {
-        'N':(
-            (1,-1),
-            (0,-1),
-            (-1,-1)
-            ),
-        'S':(
-            (1,1),
-            (0,1),
-            (-1,1)
-            ),
-        'W':(
-            (-1,1),
-            (-1,0),
-            (-1,-1)
-            ),
-        'E':(
-            (1,1),
-            (1,0),
-            (1,-1)
-            ),
-    }
+    proposals = dict()
+    for elf in elves:
+        if not any([(elf[0]+ofset[0],elf[1]+ofset[1]) in elves for ofset in ofsets]):
+            continue
+        for checkID in range(4):
+            checkGroup = checks[(checkID+turn)%4]
+            for check in checkGroup:
+                if (elf[0]+check[0],elf[1]+check[1]) in elves:
+                    break
+            else:
+                dir = checkGroup[1]
+                newPos = (elf[0]+dir[0],elf[1]+dir[1])
+                if newPos in proposals:
+                    proposals[newPos].append(elf)
+                else:
+                    proposals[newPos] = [elf]
+                break
 
-    res = False
-    for check in cheks[type]:
-        res = res or map[pos[1]+check[1]][pos[0]+check[0]]
-    return not res
-
-def count(map):
-    full = 0
-    X = set()
-    Y = set()
-    for y,row in enumerate(map):
-        for x,el in enumerate(row):
-            if el:
-                full+=1
-                X.add(x)
-                Y.add(y)
-    return (max(Y)-min(Y)+1)*(max(X)-min(X)+1)-full
+    for place,elfy in proposals.items():
+        if len(elfy) == 1:
+            moved = True
+            elves.remove(elfy[0])
+            elves.add(place)
+    return moved
+            
+def count(elves):
+    minX = min(e[0] for e in elves)
+    maxX = max(e[0] for e in elves)
+    minY = min(e[1] for e in elves)
+    maxY = max(e[1] for e in elves)
+    return(maxX-minX +1)*(maxY-minY+1)-len(elves)
 
 
 
 
 
 def task1():
-    dirDecode = {
-        'N':(0,-1),
-        'S':(0,1),
-        'W':(-1,0),
-        'E':(1,0)
-    }
-
-    turn =[
-        ['N','S','W','E'],
-        ['S','W','E','N'],
-        ['W','E','N','S'],
-        ['E','N','S','W'],
-    ]
-
-
-    elves = []
-    map = []
-    getElves(elves,map)
     
-    mask = [[0 for _ in map[0]]for _ in map]
-    
+    elves = set()
+ 
 
+    getElves(elves)
     for i in range(10):
-        wants = set()
-        for elf in elves:
-            if not validForMovement(elf.pos,map):
-                continue
-            for move in turn[i%4]:
-                if checkMovement(elf.pos,move,map):
-                    elf.wants = [elf.pos[0]+dirDecode[move][0],elf.pos[1]+dirDecode[move][1]]
-                    break
-        
-        for elf1 in elves:
-            for elf2 in elves:
-                if elf1 != elf2 and elf1.wants == elf2.wants and elf1.wants != None:     
-                    wants.add((elf1.wants[0],elf1.wants[1]))
+        moveElves(elves,i)
+    print(count(elves))
 
-       
-        for elf in elves:   
-            if elf.wants:
-                
-                if not (elf.wants[0],elf.wants[1]) in wants:
-                    elf.pos = [elf.wants[0],elf.wants[1]]
-                    elf.wants = None
-              
    
-        mask = [[0 for _ in map[0]]for _ in map]
-        map =[[False for _ in mask[0]]for _ in mask]
-       
-        for elf in elves:
-            map[elf.pos[1]][elf.pos[0]] =True
-    print(count(map))
-       
-       
-   
+def task2():
+    elves = set()
+ 
+
+    getElves(elves)
+    i = 0
+    while moveElves(elves,i):
+        i+=1
+        print(f"\r{i+1}",end="")
+    
 if __name__ == "__main__":
     task1()
+    task2()
